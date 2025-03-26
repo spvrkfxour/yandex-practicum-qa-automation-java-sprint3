8b099788633c5b59c0eb8aaf5d8b7ffe5ffcb0a6
Spark <spvrkfxour@gmail.com>
2025-03-26 15:58:24 +0300

    first commit

---
+++ src/main/java/ru/yandex/praktikum/Program.java
@@ -0,0 +1,11 @@
+package ru.yandex.praktikum;
+
+public class Program {
+
+    public static void main(String[] args) {
+    }
+
+    public boolean checkIsAdult(int age) {
+        return age >= 18;
+    }
+}
---
+++ src/test/java/ru/yandex/praktikum/ProgramTest.java
@@ -0,0 +1,36 @@
+package ru.yandex.praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+
+@RunWith(Parameterized.class)
+public class ProgramTest {
+
+    private final int age;
+    private final boolean result;
+
+    public ProgramTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {17, false},
+                {18, true},
+                {20, true},
+                {21, true},
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(age);
+        assertEquals("Метод checkIsAdult вернул на правильное значение для определения совершеннолетия", result, isAdult);
+    }
+}
